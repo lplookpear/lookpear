a777a2640050f21e172aad77a0f0813a0517e6c5
Kittichok <kittichok@anywheretogo.com>
2020-03-30 11:18:08 +0700

    ⚡ [Add] firebase connection into login and successful page

--- lib/pages/Successful.dart
+++ lib/pages/Successful.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Successful extends StatefulWidget {
   @override
@@ -37,6 +39,28 @@ class _SuccessfulState extends State<Successful> {
       ]));
     }
     return rows;
+  }
+
+  Future readDB() async {
+    final FirebaseApp app = await FirebaseApp.configure(
+      name: 'test',
+      options: FirebaseOptions(
+          googleAppID: '1:219065956997:android:abebdc4572ab97fe0547ec',
+          gcmSenderID: '219065956997',
+          apiKey: 'AIzaSyA8W2yxV_GI89UxvfwCX7fboFmc9Xf_v3k',
+          projectID: 'senior-project-b93f1',
+          databaseURL: 'https://senior-project-b93f1.firebaseio.com/'),
+    );
+
+    final FirebaseDatabase database = FirebaseDatabase(app: app);
+    var recentJobsRef =
+        database.reference().child('Mizu').orderByChild('created_at');
+    return recentJobsRef.once().then((value) {
+      if (value.value == null) return 'null';
+      return value.value;
+    }, onError: (e) {
+      return null;
+    });
   }
 
   @override
